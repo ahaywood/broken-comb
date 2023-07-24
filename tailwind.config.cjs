ef9f5c16f1f62126ec8806d25fe070f61d7a0e92
Amy Haywood Dutton <me@amyhaywood.com>
2023-07-24 12:31:12 -0500

    ✨ Set up landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				chromeWhite: '#e7eed8',
+				orangeRed: '#ff4100'
+			},
+		},
+		fontFamily: {
+			sans: ['Avenir Next', 'Avenir', 'Helvetica', 'sans-serif'],
+			mono: ['Courier Prime', 'Courier New', 'Courier', 'monospace']
+		}
 	},
 	plugins: [],
 }
